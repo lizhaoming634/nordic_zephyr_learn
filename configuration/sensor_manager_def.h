bf4a877228332e85458c2f92c5464e782ddbbe42
lizhaoming <13678462+lizhao-ming@user.noreply.gitee.com>
2025-07-20 10:39:15 +0800

    real sensor work ok

--- configuration/sensor_manager_def.h
+++ configuration/sensor_manager_def.h
@@ -9,28 +9,45 @@
  */
 const struct {} sensor_manager_def_include_once;
 
-static const struct caf_sampled_channel accel_chan[] = {
-	{
-		.chan = SENSOR_CHAN_ACCEL_X,
-		.data_cnt = 1,
-	},
-	{
-		.chan = SENSOR_CHAN_ACCEL_Y,
-		.data_cnt = 1,
-	},
-	{
-		.chan = SENSOR_CHAN_ACCEL_Z,
-		.data_cnt = 1,
-	},
+static const struct caf_sampled_channel bmi270_chan[] = {
+    {
+        .chan = SENSOR_CHAN_ACCEL_XYZ,
+        .data_cnt = 3,
+    },
+};
+
+
+
+static const struct caf_sampled_channel bme280_chans[] = {
+    {
+        .chan = SENSOR_CHAN_AMBIENT_TEMP,
+        .data_cnt = 1,
+    },
+    {
+        .chan = SENSOR_CHAN_HUMIDITY,
+        .data_cnt = 1,
+    },
+    {
+        .chan = SENSOR_CHAN_PRESS,
+        .data_cnt = 1,
+    },
 };
 
 static const struct sm_sensor_config sensor_configs[] = {
-	{
-        .dev = DEVICE_DT_GET(DT_NODELABEL(sensor_stub)),
-        .event_descr = DT_PROP(DT_NODELABEL(agg0), sensor_descr),
-        .chans = accel_chan,
-        .chan_cnt = ARRAY_SIZE(accel_chan),
-        .sampling_period_ms = 200,
+    {
+        .dev = DEVICE_DT_GET(DT_NODELABEL(bmi270)),
+        .chans = bmi270_chan,
+        .chan_cnt = ARRAY_SIZE(bmi270_chan),
+        .sampling_period_ms = 2000,
         .active_events_limit = 3,
+        .event_descr = "bmi270_accel",
+    },
+    {
+        .dev = DEVICE_DT_GET(DT_NODELABEL(bme280)),
+        .chans = bme280_chans,
+        .chan_cnt = ARRAY_SIZE(bme280_chans),
+        .sampling_period_ms = 2000,
+        .active_events_limit = 3,
+        .event_descr = "bme280_env",
     }
 };
--- src/main.c
+++ src/main.c
@@ -10,13 +10,75 @@
 
 #include <caf/events/module_state_event.h>
 #include <zephyr/logging/log.h>
+#include <zephyr/drivers/sensor.h>
 
 LOG_MODULE_REGISTER(MODULE);
+
+
+static void bmi270_init(void)
+{
+    const struct device *const dev = DEVICE_DT_GET(DT_NODELABEL(bmi270));
+	struct sensor_value full_scale, sampling_freq, oversampling;
+
+    if (dev == NULL) {
+        printk("\nError: no device found.\n");
+        return;
+    }
+
+    if (!device_is_ready(dev)) {
+        printk("\nError: device %s is not ready.\n", dev->name);
+        return;
+    }   
+
+    	/* Setting scale in G, due to loss of precision if the SI unit m/s^2
+	 * is used
+	 */
+	full_scale.val1 = 2;            /* G */
+	full_scale.val2 = 0;
+	sampling_freq.val1 = 100;       /* Hz. Performance mode */
+	sampling_freq.val2 = 0;
+	oversampling.val1 = 1;          /* Normal mode */
+	oversampling.val2 = 0;
+
+	sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_FULL_SCALE,
+			&full_scale);
+	sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ, SENSOR_ATTR_OVERSAMPLING,
+			&oversampling);
+	/* Set sampling frequency last as this also sets the appropriate
+	 * power mode. If already sampling, change to 0.0Hz before changing
+	 * other attributes
+	 */
+	sensor_attr_set(dev, SENSOR_CHAN_ACCEL_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY,
+			&sampling_freq);
+
+/* Setting scale in degrees/s to match the sensor scale */
+	full_scale.val1 = 500;          /* dps */
+	full_scale.val2 = 0;
+	sampling_freq.val1 = 100;       /* Hz. Performance mode */
+	sampling_freq.val2 = 0;
+	oversampling.val1 = 1;          /* Normal mode */
+	oversampling.val2 = 0;
+
+	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_FULL_SCALE,
+			&full_scale);
+	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ, SENSOR_ATTR_OVERSAMPLING,
+			&oversampling);
+	/* Set sampling frequency last as this also sets the appropriate
+	 * power mode. If already sampling, change sampling frequency to
+	 * 0.0Hz before changing other attributes
+	 */
+	sensor_attr_set(dev, SENSOR_CHAN_GYRO_XYZ,
+			SENSOR_ATTR_SAMPLING_FREQUENCY,
+			&sampling_freq);
+
+    printk("Found device \%s\", getting sensor data...\n", dev->name);
+}
 
 int main(void)
 {
 	int ret;
-
+	bmi270_init();
 	ret = app_event_manager_init();
 	if (ret) {
 		LOG_ERR("Event Manager not initialized, err: %d", ret);
